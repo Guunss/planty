98085e5b83ce322b80d08c68ead95d71552e642c
Gudrun Jans <guunss80@gmail.com>
2025-11-01 16:23:11 +0100

    Revue CSS mobile

--- app/public/wp-content/themes/oceanwp-child/footer.php
+++ app/public/wp-content/themes/oceanwp-child/footer.php
@@ -7,27 +7,28 @@
 
 ?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
-	<?php do_action( 'ocean_after_main' ); ?>
+<?php do_action('ocean_after_main'); ?>
 
-	<?php do_action( 'ocean_before_footer' ); ?>
-
-	<footer>
-        <nav>
-            <a href="/">Mentions légales</a>
-        </nav>
-    </footer>
-
+<?php do_action('ocean_before_footer'); ?>
+<!-- custom footer -->
+<footer>
+    <nav>
+        <a href="/">Mentions légales</a>
+    </nav>
+</footer>
+<!-- fin custom footer -->
 </div><!-- #wrap -->
 
-<?php do_action( 'ocean_after_wrap' ); ?>
+<?php do_action('ocean_after_wrap'); ?>
 
 </div><!-- #outer-wrap -->
 
-<?php do_action( 'ocean_after_outer_wrap' ); ?>
+<?php do_action('ocean_after_outer_wrap'); ?>
 
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- app/public/wp-content/themes/oceanwp-child/header.php
+++ app/public/wp-content/themes/oceanwp-child/header.php
@@ -7,53 +7,55 @@
 
 ?>
 <!DOCTYPE html>
-<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+<html class="<?php echo esc_attr(oceanwp_html_classes()); ?>" <?php language_attributes(); ?>>
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?> <?php oceanwp_schema_markup( 'html' ); ?>>
+<body <?php body_class(); ?> <?php oceanwp_schema_markup('html'); ?>>
 
-	<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
 
-	<?php do_action( 'ocean_before_outer_wrap' ); ?>
+    <?php do_action('ocean_before_outer_wrap'); ?>
 
-	<div id="outer-wrap" class="site clr">
+    <div id="outer-wrap" class="site clr">
+        <a class="skip-link screen-reader-text"
+            href="#main"><?php echo esc_html(oceanwp_theme_strings('owp-string-header-skip-link', false)); ?></a>
 
-		<a class="skip-link screen-reader-text" href="#main"><?php echo esc_html( oceanwp_theme_strings( 'owp-string-header-skip-link', false ) ); ?></a>
+        <?php do_action('ocean_before_wrap'); ?>
 
-		<?php do_action( 'ocean_before_wrap' ); ?>
+        <div id="wrap" class="clr">
+            <!-- custom header -->
 
-		<div id="wrap" class="clr">
-
-			<header>
+            <header>
                 <div class="logo">
-                <a href="http://planty.local">
-                    <div class="logo">
-                        <img src="http://planty.local/wp-content/uploads/2025/10/Logo-source.png"/>           
-                    </div>
-                </a>
+                    <a href="http://planty.local">
+                        <div class="logo">
+                            <img src="http://planty.local/wp-content/uploads/2025/10/Logo-source.png" />
+                        </div>
+                    </a>
                 </div>
                 <nav>
                     <div class="main-pages">
-                    <div class="nav-item">
-                        <a href="/precommande">Nous rencontrer</a>
-                    </div>
-                    <?php if(is_user_logged_in()) { ?>
-                    <div class="nav-item">
-                        <a href="/wp-admin">Admin</a>
-                    </div>
-                    <?php } ?>
+                        <div class="nav-item">
+                            <a href="/precommande">Nous rencontrer</a>
+                        </div>
+                        <?php if (is_user_logged_in()) { ?>
+                            <div class="nav-item">
+                                <a href="/wp-admin">Admin</a>
+                            </div>
+                        <?php } ?>
                     </div>
                     <div class="nav-item dark">
                         <a href="/commander">Commander</a>
                     </div>
                 </nav>
             </header>
+            <!-- custom header fin -->
+            <main id="main" class="site-main clr" <?php oceanwp_schema_markup('main'); ?> role="main">
 
-			<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?> role="main">
-
-				<?php do_action( 'ocean_page_header' ); ?>
+                <?php do_action('ocean_page_header'); ?>
